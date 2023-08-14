acf3785beb9d8132e4c8a1d8ede055d712acb430
MaviYoki <mariavitoriapossi@hotmail.com>
2023-08-14 20:58:45 -0300

    chore: added method 'Descending' and adjustment of 'Exit' of Menu()

--- Program.cs
+++ Program.cs
@@ -13,54 +13,86 @@ namespace Stopwatch
         static void Menu()
         {
             Console.Clear();
+            Console.WriteLine(" ");
             Console.WriteLine("S = seconds");
             Console.WriteLine("M = minutes");
             Console.WriteLine("0 = exit");
             Console.WriteLine("Enter the time to start the stopwatch");
+
             try
             {
                 var data = Console.ReadLine().ToLower();
 
+                if (data == "0")
+                {
+                    Console.WriteLine("Exit");
+                    return;
+                }
+
                 var type = char.Parse(data.Substring(data.Length - 1, 1));
+                Console.WriteLine(type);
+                Thread.Sleep(2000);
                 var time = int.Parse(data.Substring(0, data.Length - 1));
+
+
+
+                Console.WriteLine("D - Descending count");
+                Console.WriteLine("G - Growing count");
+
+                var count = Console.ReadLine().ToUpper();
+
 
                 var multiplier = 1;
 
                 if (type == 'm')
                     multiplier = 60;
 
-                if (time == 0)
-                {
-                    Console.WriteLine("Exit");
-                    return;
-                }
+                if (count == "D")
+                    Descending(time * multiplier);
 
-                Start(time * multiplier);
+                if (count == "G")
+                    Growing(time * multiplier);
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Invalid Input");
             }
 
-            static void Start(int time)
+        }
+        static void Growing(int time)
+        {
+            var currentTime = 0;
+
+            while (currentTime != time)
             {
-                var currentTime = 0;
-
-                while (currentTime != time)
-                {
-                    Console.Clear();
-                    currentTime++;
-                    Console.WriteLine(currentTime);
-                    Thread.Sleep(1000);
-                }
                 Console.Clear();
-
-                Console.WriteLine("Finished stopwatch...");
-                Thread.Sleep(2000);
-                Menu();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
             }
+            Console.Clear();
 
+            Console.WriteLine("Finished stopwatch...");
+            Thread.Sleep(2000);
+            Menu();
+        }
 
+        static void Descending(int time)
+        {
+            var currentTime = 0;
+
+            while (time != currentTime)
+            {
+                Console.Clear();
+                time--;
+                Console.WriteLine(time);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+
+            Console.WriteLine("Finished stopwatch...");
+            Thread.Sleep(2000);
+            Menu();
         }
     }
 }
